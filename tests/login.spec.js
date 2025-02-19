6ab309dbda831f1793fa5a35c800bdec398741f5
Fernando Papito <papitodev@proton.me>
2025-02-19 17:52:05 -0300

    redis

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -5,6 +5,9 @@ import { obterCodigo2FA } from '../support/db';
 import { LoginPage } from '../pages/LoginPage';
 import { DashPage } from '../pages/DashPage';
 
+import { cleanJobs, getJob } from '../support/redis';
+
+
 test('Não deve logar quando o código de autenticacão é inválido', async ({ page }) => {
   const loginPage = new LoginPage(page)
 
@@ -31,19 +34,21 @@ test('Deve acessar a conta do usuário', async ({ page }) => {
     senha: '147258'
   }
   
+  await cleanJobs()
+
   await loginPage.acessaPagina()
   await loginPage.informaCpf(usuario.cpf)
   await loginPage.informaSenha(usuario.senha)
 
-  // temporário
-  await page.waitForTimeout(3000)
-  const codigo = await obterCodigo2FA()
+  // checkpoint
+  await page.getByRole('heading', {name: 'Verificação em duas etapas'})
+    .waitFor({timeout: 3000})
+
+  const codigo = await getJob()
+
+  // const codigo = await obterCodigo2FA(usuario.cpf)
 
   await loginPage.informe2FA(codigo)
 
-  // temporário
-  await page.waitForTimeout(2000)
-
-  expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')
-  
+  await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00')  
 });
